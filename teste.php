0912a6a760731e1dc2b79abdce6e0c8241082ea9
Igor <igorvittorello@hotmail.com>
2020-11-23 22:17:07 -0300

    Udemy

--- teste.php
+++ teste.php
@@ -26,4 +26,32 @@ $arrCombo = array(
         </form>
 
     </body>
-</html
+</html*/>
+
+<br>
+
+<html>
+   
+   <head>
+      <title>Arithmetical Operators</title>
+   </head>
+   
+   <body>
+   
+      <?php
+         $a = 10;
+         $b = 20;
+         
+         /* If condition is true then assign a to result otheriwse b */
+         $result = ($a > $b ) ? $a :$b;
+         
+         echo "TEST1 : Value of result is $result<br/>";
+         
+         /* If condition is true then assign a to result otheriwse b */
+         $result = ($a < $b ) ? $a :$b;
+         
+         echo "TEST2 : Value of result is $result<br/>";
+      ?>
+   
+   </body>
+</html>
